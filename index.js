91f5f4ebb1f39794e5500e265d0aeac4795d24dc
songhee24 <akaratel61@gmail.com>
2023-08-08 17:04:11 +0600

    mongoose config

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://azamat:123@cluster0.fxlagp4.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("mongodb successfully connected");
+  })
+  .catch(() => {
+    console.log("mongodb got an error");
+  });
 
 const app = express();
 app.use(express.json());
