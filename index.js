284531748ce6d8a1d888d5bb2b1ff34ca9c8c63f
songhee24 <akaratel61@gmail.com>
2023-08-09 21:28:58 +0600

    usage for create posts

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { loginValidation, registerValidation } from "./validations.js";
 
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 mongoose
   .connect(
     "mongodb+srv://azamat:123@cluster0.fxlagp4.mongodb.net/blog?retryWrites=true&w=majority"
@@ -22,6 +23,12 @@ app.use(express.json());
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+// app.get('/posts', PostController.getAll)
+// app.get('/posts/:id', PostController.getOne)
+app.post("/posts", PostController.create);
+// app.delete('/posts', PostController.remove)
+// app.patch('/posts', PostController.update)
 
 app.listen(4444, (err) => {
   if (err) {
