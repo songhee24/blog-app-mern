2ab34e71247016ee8949af4222f1636d8fe6b9a6
songhee24 <akaratel61@gmail.com>
2023-08-09 19:44:30 +0600

    handle register errors

--- index.js
+++ index.js
@@ -21,25 +21,32 @@ const app = express();
 app.use(express.json());
 
 app.post("/auth/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserScheme({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: passwordHash,
+    });
+
+    const user = await doc.save();
+
+    return res.json(user);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Не удалось зарегистироваться",
+    });
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserScheme({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash: passwordHash,
-  });
-
-  const user = await doc.save();
-
-  return res.json(user);
 });
 
 app.listen(4444, (err) => {
