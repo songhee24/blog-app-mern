48a14048a31d5ca94a933f54dd1b3e33fd3b0380
songhee24 <akaratel61@gmail.com>
2023-08-09 21:50:14 +0600

    get all posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,15 @@
 import PostScheme from "../models/Post.js";
 
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostScheme.find();
+    res.json(posts);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Не удалось получить статьи" });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostScheme({
