647c685edf88a5aa4c1c779ef96d9dad6bc11235
songhee24 <akaratel61@gmail.com>
2023-08-09 22:01:53 +0600

    get one post with mongoose logic

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,8 +2,45 @@ import PostScheme from "../models/Post.js";
 
 export const getAll = async (req, res) => {
   try {
-    const posts = await PostScheme.find();
+    const posts = await PostScheme.find().populate("user").exec();
     res.json(posts);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Не удалось получить статьи" });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostScheme.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: "after",
+      }
+    )
+      .then((doc) => {
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json(doc);
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(e);
+          return res
+            .status(500)
+            .json({ message: "Не удалось получить статью" });
+        }
+      });
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: "Не удалось получить статьи" });
