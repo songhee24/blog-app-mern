9ce3ef53550b76c537e9312bc32ed9577a10a163
songhee24 <akaratel61@gmail.com>
2023-08-09 20:16:36 +0600

    user login logic was created

--- index.js
+++ index.js
@@ -56,6 +56,42 @@ app.post("/auth/register", registerValidation, async (req, res) => {
   }
 });
 
+app.post("/auth/login", registerValidation, async (req, res) => {
+  try {
+    const user = await UserScheme.findOne({ email: req.body.email });
+    if (!user) {
+      return req.status(404).json({
+        message: "Пользователь не найден",
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(404).json({
+        message: "Невереный логин или пароль",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      { expiresIn: "30d" }
+    );
+    return res.json({ ...user._doc, token });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Не удалось авторизоваться",
+    });
+  }
+});
+
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err);
