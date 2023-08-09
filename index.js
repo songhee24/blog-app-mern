0767120d0084e946e053f5c50c486baab8b64342
songhee24 <akaratel61@gmail.com>
2023-08-09 18:56:09 +0600

    saving user doc

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { registerValidation } from "./validations/auth.js";
 import { validationResult } from "express-validator";
 import UserScheme from "./models/User.js";
+import bcrypt from "bcrypt";
 
 mongoose
   .connect(
@@ -19,20 +20,26 @@ mongoose
 const app = express();
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
   const doc = new UserScheme({
     email: req.body.email,
     fullName: req.body.fullName,
     avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.password,
+    passwordHash: passwordHash,
   });
 
-  return res.json({ success: true });
+  const user = await doc.save();
+
+  return res.json(user);
 });
 
 app.listen(4444, (err) => {
