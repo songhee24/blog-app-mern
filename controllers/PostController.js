f95e5a6b729d2df4ff7ba620edeccc24d2663047
songhee24 <akaratel61@gmail.com>
2023-08-09 21:24:02 +0600

    post doc create controller

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,19 @@
-export const create = (req, res) => {
+import PostScheme from "../models/Post.js";
+
+export const create = async (req, res) => {
   try {
-  } catch (e) {}
+    const doc = new PostScheme({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+    return res.json(post);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Не удалось создать статью" });
+  }
 };
