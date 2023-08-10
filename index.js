c4f73b8fe8d8afb5568f29711c5c4962f91c718d
songhee24 <akaratel61@gmail.com>
2023-08-10 23:15:24 +0600

    multer for upload image

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import {
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
 import * as PostController from "./controllers/PostController.js";
+import multer from "multer";
 mongoose
   .connect(
     "mongodb+srv://azamat:123@cluster0.fxlagp4.mongodb.net/blog?retryWrites=true&w=majority"
@@ -24,9 +25,26 @@ mongoose
 const app = express();
 app.use(express.json());
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `uploads/${req.file.originalname}`,
+  });
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
