ee89ba7222112dc141c95fe1b2c03018cf599f7f
songhee24 <akaratel61@gmail.com>
2023-08-15 21:01:17 +0600

    tags update and create

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -100,7 +100,7 @@ export const update = async (req, res) => {
         title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(","),
       }
     );
     res.json({ success: true });
